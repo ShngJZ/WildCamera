a9cd8216958ead66044ac30cef9ac86ec1a60bf2
ShngJZ <zhusheng@msu.edu>
2023-12-25 11:25:48 -0500

    add torch.hub

--- hubconf.py
+++ hubconf.py
@@ -5,7 +5,7 @@ from WildCamera.newcrfs.newcrf_incidencefield import NEWCRFIF
 def WildCamera(pretrained=True):
     model = NEWCRFIF(version='large07', pretrained=None)
     if pretrained:
-        pretrained_resource = "url::https://huggingface.co/datasets/Shengjie/WildCamera/resolve/main/checkpoint/wild_camera_all.pth"
+        pretrained_resource = "https://huggingface.co/datasets/Shengjie/WildCamera/resolve/main/checkpoint/wild_camera_all.pth"
         state_dict = torch.hub.load_state_dict_from_url(pretrained_resource, map_location='cpu')
         model.load_state_dict(state_dict, strict=True)
     return model
